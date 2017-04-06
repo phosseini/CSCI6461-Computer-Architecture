51a69ba09c93ee8d7511a614d4c48f188d5f7b09
Pedram <pdr.hosseini@gmail.com>
2017-04-05 22:29:03 -0400

    Minor change

--- src/alu/instruction/CHK.java
+++ src/alu/instruction/CHK.java
@@ -7,44 +7,30 @@ import util.MachineFaultException;
 import util.StringUtil;
 
 public class CHK extends AbstractInstruction {
-	
+
 	int num;
 	int devId;
 
 	@Override
 	public void execute(String instruction, Registers registers, MCU mcu) throws MachineFaultException {
-		// TODO Auto-generated method stub
 		this.num = StringUtil.binaryToDecimal(instruction.substring(6, 8));
 		this.devId = StringUtil.binaryToDecimal(instruction.substring(11, 16));
-		
+
 		if (this.devId == Const.DevId.KEYBOARD.getValue()) {
-			
-                
-                registers.setRnByNum(this.num, 1);
-               
-           
+			registers.setRnByNum(this.num, 1);
 		}
-		
+
 		if (this.devId == Const.DevId.CARD.getValue()) {
-			
-                registers.setRnByNum(this.num, 1);
-               
-        }
-        
-		
-		
-		if (this.devId == Const.DevId.PRINTER.getValue()) {
-			
-            	
-                registers.setRnByNum(this.num, 1);
-               
+			registers.setRnByNum(this.num, 1);
 		}
-	}	
-	
+
+		if (this.devId == Const.DevId.PRINTER.getValue()) {
+			registers.setRnByNum(this.num, 1);
+		}
+	}
 
 	@Override
 	public String getExecuteMessage() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
