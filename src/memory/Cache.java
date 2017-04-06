08c68df475794a8c9233b1fc3cc880a26d844d13
Pedram <pdr.hosseini@gmail.com>
2017-04-06 02:37:54 -0400

    minor

--- src/memory/Cache.java
+++ src/memory/Cache.java
@@ -6,48 +6,48 @@ import util.Const;
 
 public class Cache {
 
-    public class CacheLine {
+	public class CacheLine {
 
-        int tag;
-        int data;
+		int tag;
+		int data;
 
-        public CacheLine(int tag, int data) {
-            this.tag = tag;
-            this.data = data;
-        }
+		public CacheLine(int tag, int data) {
+			this.tag = tag;
+			this.data = data;
+		}
 
-        public int getTag() {
-            return this.tag;
-        }
+		public int getTag() {
+			return this.tag;
+		}
 
-        public void setTag(int tag) {
-            this.tag = tag;
-        }
+		public void setTag(int tag) {
+			this.tag = tag;
+		}
 
-        public int getData() {
-            return data;
-        }
+		public int getData() {
+			return data;
+		}
 
-        public void setData(int data) {
-            this.data = data;
-        }
-    }
+		public void setData(int data) {
+			this.data = data;
+		}
+	}
 
-    LinkedList<CacheLine> cacheLines;
+	LinkedList<CacheLine> cacheLines;
 
-    public Cache() {
-        this.cacheLines = new LinkedList<CacheLine>();
-    }
+	public Cache() {
+		this.cacheLines = new LinkedList<CacheLine>();
+	}
 
-    public LinkedList<CacheLine> getCacheLines() {
-        return cacheLines;
-    }
+	public LinkedList<CacheLine> getCacheLines() {
+		return cacheLines;
+	}
 
-    public void add(int address, int value) {
-        if (this.cacheLines.size() >= Const.CACHE_LINES) {
-            this.cacheLines.removeLast();
-        }
-        this.cacheLines.addFirst(new CacheLine(address, value));
-    }
+	public void add(int address, int value) {
+		if (this.cacheLines.size() >= Const.CACHE_LINES) {
+			this.cacheLines.removeLast();
+		}
+		this.cacheLines.addFirst(new CacheLine(address, value));
+	}
 
 }
--- src/memory/MCU.java
+++ src/memory/MCU.java
@@ -19,176 +19,174 @@ import util.Const;
  */
 public class MCU {
 
-    /**
-     * 16 bit words
-     */
-    ArrayList<Integer> memory;
+	/**
+	 * 16 bit words
+	 */
+	ArrayList<Integer> memory;
 
-    /**
-     * 16 block fully associative, unified cache
-     */
-    Cache cache;
+	/**
+	 * 16 block fully associative, unified cache
+	 */
+	Cache cache;
 
-    String printerBuffer;
-    String keyboardBuffer;
-    String cardBuffer;
+	String printerBuffer;
+	String keyboardBuffer;
+	String cardBuffer;
 
-    public String getPrinterBuffer() {
-        return printerBuffer;
-    }
+	public String getPrinterBuffer() {
+		return printerBuffer;
+	}
 
-    public void setPrinterBuffer(String printerBuffer) {
-        this.printerBuffer = printerBuffer;
-    }
+	public void setPrinterBuffer(String printerBuffer) {
+		this.printerBuffer = printerBuffer;
+	}
 
-    public String getKeyboardBuffer() {
-        return keyboardBuffer;
-    }
+	public String getKeyboardBuffer() {
+		return keyboardBuffer;
+	}
 
-    public void setKeyboardBuffer(String keyboardBuffer) {
-        this.keyboardBuffer = keyboardBuffer;
-    }
+	public void setKeyboardBuffer(String keyboardBuffer) {
+		this.keyboardBuffer = keyboardBuffer;
+	}
 
-    public String getCardBuffer() {
-        return cardBuffer;
-    }
+	public String getCardBuffer() {
+		return cardBuffer;
+	}
 
-    public void setCardBuffer(String cardBuffer) {
-        this.cardBuffer = cardBuffer;
-    }
+	public void setCardBuffer(String cardBuffer) {
+		this.cardBuffer = cardBuffer;
+	}
 
-    public Cache getCache() {
-        return cache;
-    }
+	public Cache getCache() {
+		return cache;
+	}
 
-    /**
-     * initialize the MCU, all memories set to 0, memories size 2048.
-     */
-    public MCU() {
-        this.memory = new ArrayList<Integer>(Const.MEMORY_WORDS_BOUND);
-        for (int i = 0; i < Const.MEMORY_WORDS_BOUND; i++) {
-            this.memory.add(0);
-        }
-        this.cache = new Cache();
-        // System.out.println("MCU init with a size of " + this.memory.size());
-    }
+	/**
+	 * initialize the MCU, all memories set to 0, memories size 2048.
+	 */
+	public MCU() {
+		this.memory = new ArrayList<Integer>(Const.MEMORY_WORDS_BOUND);
+		for (int i = 0; i < Const.MEMORY_WORDS_BOUND; i++) {
+			this.memory.add(0);
+		}
+		this.cache = new Cache();
+		// System.out.println("MCU init with a size of " + this.memory.size());
+	}
 
-    /**
-     * expand the memory size to 4096
-     */
-    public void expandMemorySize() {
-        if (this.memory != null && this.memory.size() > 0) {
-            this.memory.ensureCapacity(Const.MEMORY_WORDS_BOUND_EXPANDED);
-            for (int currentSize = memory.size(); currentSize < Const.MEMORY_WORDS_BOUND_EXPANDED; currentSize++) {
-                this.memory.add(0);
-            }
-        }
-        System.out.println("memory size has been expanded to " + memory.size());
-    }
-    
+	/**
+	 * expand the memory size to 4096
+	 */
+	public void expandMemorySize() {
+		if (this.memory != null && this.memory.size() > 0) {
+			this.memory.ensureCapacity(Const.MEMORY_WORDS_BOUND_EXPANDED);
+			for (int currentSize = memory.size(); currentSize < Const.MEMORY_WORDS_BOUND_EXPANDED; currentSize++) {
+				this.memory.add(0);
+			}
+		}
+		System.out.println("memory size has been expanded to " + memory.size());
+	}
 
+	/**
+	 * @return current size of the memory
+	 */
+	public int getCurrentMemorySize() {
+		if (this.memory != null) {
+			return this.memory.size();
+		}
+		return 0;
+	}
 
-    /**
-     * @return current size of the memory
-     */
-    public int getCurrentMemorySize() {
-        if (this.memory != null) {
-            return this.memory.size();
-        }
-        return 0;
-    }
+	/**
+	 * 
+	 * Using the address to fetch a word directly from memory.
+	 * 
+	 * @param address
+	 * @return a word from memory
+	 * 
+	 */
+	public int fetchFromMemory(int address) {
+		return this.memory.get(address);
+	}
 
-    /**
-     * 
-     * Using the address to fetch a word directly from memory.
-     * 
-     * @param address
-     * @return a word from memory
-     * 
-     */
-    public int fetchFromMemory(int address) {
-        return this.memory.get(address);
-    }
+	/**
+	 * 
+	 * Store directly into memory using address and value.
+	 * 
+	 * @param address
+	 * @param value
+	 */
+	public void storeIntoMemory(int address, int value) {
+		if (this.memory != null) {
+			this.memory.set(address, value);
+		}
+	}
 
-    /**
-     * 
-     * Store directly into memory using address and value.
-     * 
-     * @param address
-     * @param value
-     */
-    public void storeIntoMemory(int address, int value) {
-        if (this.memory != null) {
-            this.memory.set(address, value);
-        }
-    }
+	/**
+	 * 
+	 * fetch a word from cache. If the word is not in cache, fetch it from
+	 * memory, then store it into cache.
+	 * 
+	 * @param address
+	 * @return
+	 */
+	public int fetchFromCache(int address) {
+		for (CacheLine line : cache.getCacheLines()) { // check every block
+														// whether the tag is
+														// already exist
+			if (address == line.getTag()) {
+				return line.getData(); // tag exist, return the data of the
+										// block
+			}
+		}
+		// tag not exist
+		int value = fetchFromMemory(address);
+		cache.add(address, value);
+		return value;
+	}
 
-    /**
-     * 
-     * fetch a word from cache. If the word is not in cache, fetch it from
-     * memory, then store it into cache.
-     * 
-     * @param address
-     * @return
-     */
-    public int fetchFromCache(int address) {
-        for (CacheLine line : cache.getCacheLines()) { // check every block
-                                                       // whether the tag is
-                                                       // already exist
-            if (address == line.getTag()) {
-                return line.getData(); // tag exist, return the data of the
-                                       // block
-            }
-        }
-        // tag not exist
-        int value = fetchFromMemory(address);
-        cache.add(address, value);
-        return value;
-    }
+	/**
+	 * 
+	 * store into cache with replacement. Also store into memory simultaneously.
+	 * 
+	 * @param address
+	 * @param value
+	 */
+	public void storeIntoCache(int address, int value) {
+		storeIntoMemory(address, value);
+		for (CacheLine line : cache.getCacheLines()) { // check every block the
+														// tag is already exist
+			if (address == line.getTag()) {
+				line.setData(value); // replace the block
+				return;
+			}
+		}
+		// tag not exist
+		cache.add(address, value);
+	}
 
-    /**
-     * 
-     * store into cache with replacement. Also store into memory simultaneously.
-     * 
-     * @param address
-     * @param value
-     */
-    public void storeIntoCache(int address, int value) {
-        storeIntoMemory(address, value);
-        for (CacheLine line : cache.getCacheLines()) { // check every block the
-                                                       // tag is already exist
-            if (address == line.getTag()) {
-                line.setData(value); // replace the block
-                return;
-            }
-        }
-        // tag not exist
-        cache.add(address, value);
-    }
+	/**
+	 * Load from ROM and store the instructions after octal 10
+	 */
+	// public void loadMemoryFromROM() {
+	// HashMap<String, Integer> rom = Const.ROM;
+	// // System.out.println("read from the ROM");
+	// if (rom != null) {
+	// for (Map.Entry<String, Integer> entry : rom.entrySet()) {
+	// int address = Integer.parseInt(entry.getKey());
+	// int value = entry.getValue();
+	// storeIntoCache(address, value);
+	// }
+	// }
+	// }
 
-    /**
-     * Load from ROM and store the instructions after octal 10
-     */
-//    public void loadMemoryFromROM() {
-//        HashMap<String, Integer> rom = Const.ROM;
-//        // System.out.println("read from the ROM");
-//        if (rom != null) {
-//            for (Map.Entry<String, Integer> entry : rom.entrySet()) {
-//                int address = Integer.parseInt(entry.getKey());
-//                int value = entry.getValue();
-//                storeIntoCache(address, value);
-//            }
-//        }
-//    }
-    
-    public void loadProgram(HashMap<String, Integer> program){
-        if(program != null){
-            for (Map.Entry<String, Integer> entry : program.entrySet()) {
-                int address = Integer.parseInt(entry.getKey());
-                int value = entry.getValue();
-                storeIntoCache(address, value);
-            }
-        }
-    }
+	public void loadProgram(HashMap<String, Integer> program) {
+		if (program != null) {
+			for (Map.Entry<String, Integer> entry : program.entrySet()) {
+				int address = Integer.parseInt(entry.getKey());
+				int value = entry.getValue();
+				storeIntoCache(address, value);
+			}
+		}
+	}
 
 }
